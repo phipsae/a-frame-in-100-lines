c6917457fccd3e6f98327d527de7f4c6637c20a2
phipsae <philip.krause88@yahoo.de>
2024-05-20 17:32:19 +0200

    terst

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,6 +12,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
   }
 
+  console.log('message', message);
+
   const client = createPublicClient({
     chain: baseSepolia,
     transport: http(),
@@ -21,35 +23,18 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     address: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
   });
 
-  // if (!isValid) {
-  //   return new NextResponse('Message not valid', { status: 500 });
-  // }
-
-  // const text = message.input || '';
-  // let state = {
-  //   page: 0,
-  // };
-  // try {
-  //   state = JSON.parse(decodeURIComponent(message.state?.serialized));
-  // } catch (e) {
-  //   console.error(e);
-  // }
-
-  // SVG code
-  const svgCode = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="1000" viewBox="0 0 1000 1000" fill="none">
-<rect width="1000" height="1000" fill="black"/>
-<ellipse cx="499.836" cy="499.835" rx="372" ry="372" transform="rotate(90.5938 499.836 499.835)" fill="url(#paint0_linear_1_57)"/>
-<path d="M131.944 164C149.648 164 164 149.673 164 132C164 114.327 149.648 100 131.944 100C115.148 100 101.368 112.896 100 129.31H142.37V134.69H100C101.368 151.104 115.148 164 131.944 164Z" fill="#F7E582"/>
-<defs>
-<linearGradient id="paint0_linear_1_57" x1="875.281" y1="495.473" x2="124.015" y2="503.26" gradientUnits="userSpaceOnUse">
-<stop stop-color="#AD8B27"/>
-<stop offset="0.479167" stop-color="#F7E582"/>
-<stop offset="1" stop-color="#AD8B27"/>
-</linearGradient>
-</defs>
-</svg>
-  `;
+  const svgCode = `<svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" width="1000" height="1000"  fill="none">
+  <rect width="1000" height="1000" fill="black"/>
+  <ellipse cx="499.836" cy="499.835" rx="372" ry="372" transform="rotate(90.5938 499.836 499.835)" fill="url(#paint0_linear_1_57)"/>
+  <path d="M131.944 164C149.648 164 164 149.673 164 132C164 114.327 149.648 100 131.944 100C115.148 100 101.368 112.896 100 129.31H142.37V134.69H100C101.368 151.104 115.148 164 131.944 164Z" fill="#F7E582"/>
+  <defs>
+  <linearGradient id="paint0_linear_1_57" x1="875.281" y1="495.473" x2="124.015" y2="503.26" gradientUnits="userSpaceOnUse">
+  <stop stop-color="#AD8B27"/>
+  <stop offset="0.479167" stop-color="#F7E582"/>
+  <stop offset="1" stop-color="#AD8B27"/>
+  </linearGradient>
+  </defs>
+  </svg>`;
 
   const svgDataUrl = svgToDataURL(svgCode);
 
