71806b7aca64ef443bd6f8470facf1027858de83
phipsae <philip.krause88@yahoo.de>
2024-05-20 15:47:17 +0200

    .

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -37,11 +37,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   // SVG code
   const svgCode = `
-  <svg width="200" height="200" viewBox="-100 -100 200 200">
-  <polygon points="0,0 80,120 -80,120" fill="#234236" />
-  <polygon points="0,-40 60,60 -60,60" fill="#0C5C4C" />
-  <polygon points="0,-80 40,0 -40,0" fill="#38755B" />
-  <rect x="-20" y="120" width="40" height="30" fill="brown" />
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100" height="100" fill="red" />
+  <circle cx="50" cy="10" r="0" />
 </svg>
   `;
 
