37d890231daf026195da0f288a0af63bf2b405d3
phipsae <philip.krause88@yahoo.de>
2024-05-20 15:44:39 +0200

    .

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -38,7 +38,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   // SVG code
   const svgCode = `
     <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="50" cy="50" r="50" />
+      <circle cx="50" cy="10" r="10" />
     </svg>
   `;
 
