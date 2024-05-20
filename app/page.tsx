7859dd1d9dbdefbc89036d18b808a102b1d7ae65
phipsae <philip.krause88@yahoo.de>
2024-05-20 12:52:37 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
+      label: 'Story time TEST!!!',
     },
     {
       action: 'tx',
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>zizzamia.xyz</h1>
+      <h1>zizzamia.xyz, PHIPSAE</h1>
     </>
   );
 }
